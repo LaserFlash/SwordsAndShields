0ee9486c5b82ea49f0e7d55fdbc9f46f019cba8d
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-08-22 19:13:16 +1200

    GUI states

--- src/SwordsAndShields/ui/GUITurnState.java
+++ src/SwordsAndShields/ui/GUITurnState.java
@@ -1,4 +1,48 @@
 package SwordsAndShields.ui;
 
-public class GUITurnState {
+public enum GUITurnState {
+    Create {
+        @Override
+        public GUITurnState nextMajorState() {
+            return Move_Rotate;
+        }
+    },
+    Create_Rotate {
+        @Override
+        public GUITurnState nextMajorState() {
+            return Move_Rotate;
+        }
+    },
+    Move_Rotate {
+        @Override
+        public GUITurnState nextMajorState() {
+            return Create;
+        }
+    },
+    Move {
+        @Override
+        public GUITurnState nextMajorState() {
+            return Create;
+        }
+    },
+    Rotate {
+        @Override
+        public GUITurnState nextMajorState() {
+            return Create;
+        }
+    },
+    Reactions {
+        @Override
+        public GUITurnState nextMajorState() {
+            return Move_Rotate;
+        }
+    },
+    GameOver {
+        @Override
+        public GUITurnState nextMajorState() {
+            return null;
+        }
+    };
+
+    public abstract GUITurnState nextMajorState();
 }
